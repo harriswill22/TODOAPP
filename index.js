48248111ff24cc08bee9a66cc808002a43ae6dbf
Will Harris <Harriswill22@yahoo.com>
2018-11-05 10:19:47 -0500

    adds package json

---
+++ index.js
@@ -0,0 +1,8 @@
+const personName = 'Will'
+console.log(`hello ${personName}`)
+// console.log('that should not have worked.');
+function myName() {  
+    if (personName === 'Will')
+    console.log("thats my name")
+}
+myName();
