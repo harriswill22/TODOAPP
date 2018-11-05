9aa8fb9bd251339289f518173b0d95136be08234
Will Harris <Harriswill22@yahoo.com>
2018-11-05 11:25:44 -0500

    example of grabbing one row

--- index.js
+++ index.js
@@ -7,7 +7,44 @@ const db = pgp({
     database: 'node-todo-app-db'
 });
 
-db.any('select * from todos')
-    .then(results => {
-        console.log(results);
-    })
+
+
+// example of grabbing all the rows
+// db.any('select * from todos')
+//     .then(results => {
+//         console.log(results);
+//     })
+
+// example of grabbing one row
+db.one('select * from todos where id = 10;')
+.catch(err => {
+    return{
+        name: 'No Todo Found.'
+    };
+})
+
+.then(results => {
+    console.log(results);
+})
+
+
+
+
+// example of adding a row
+
+
+
+
+
+// example of updating a row
+
+
+
+
+
+
+
+// example of deleting a row
+
+
+
