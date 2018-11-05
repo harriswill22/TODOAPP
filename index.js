9ab0b977a8c0ff1743c4eff1452156ad9c1423ae
Will Harris <Harriswill22@yahoo.com>
2018-11-05 11:08:45 -0500

    creates example

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const personName = 'Will'
-console.log(`hello ${personName}`)
-// console.log('that should not have worked.');
-function myName() {  
-    if (personName === 'Will')
-    console.log("thats my name")
-}
-myName();
+// const pgPromise = require('pg-promise')();
+// const pgp = pgPromise();
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'node-todo-app-db'
+});
+
+db.any('select * from todos')
+    .then(results => {
+        console.log(results);
+    })
