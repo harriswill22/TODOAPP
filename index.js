e3507d2d171722684c9ed287f5632baba7e62015
Will Harris <Harriswill22@yahoo.com>
2018-11-07 11:39:12 -0500

    adds CRUD

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ require('dotenv').config();
 const Todo = require('./models/Todo')
 const User = require('./models/User')
 
+// ========================================================
 
+// CRUD
+
+// ========================================================
+
+// CREATE
+
+
+// Todo.addRow('shower the dog ', false)
+// .then(result => {
+// console.log(result)
+// })
+
+// ========================================================
+// RETRIEVE 
+// Todo.getAll()
+// .then(results => {
+// console.log(results);
+// console.log('All TODOS');
+// })
+
+
+// Todo.getByID(2)
+//     .then(todoID => {
+//         console.log(todoID)
+//     });
+
+
+// ========================================================
+// UPDATE
 
 
 // User.updateName(2, 'shelly')
@@ -11,116 +41,77 @@ const User = require('./models/User')
 // })
 
 
-User.getById(2)
-    .then(userFromDB => {
-        console.log(userFromDB);
-    });
-
-
-
-
-// addRow('walk the cat ', false)
-//     .catch(err => {
-//         console.log(err);
-//     })
-//     .then(result => {
-//         console.log(result)
-//     })
-
-
-
-
-// Retrieve
-// function getAll() {
-//     return db.any('select * from todos')
-// }
-// User.getAll()
-// .then(results => {
-// console.log(results);
-// console.log('All the Users');
-// })
-
-
-// example of grabbing one row
-// function getByID(id) {
-//     return db.one(`select * from todos where id = $1`,[id])
-//         .catch(err => {
-//             return {
-//                 name: 'No Todo Found.'
-//             };
-//         })
-// }
-
 // Todo.getByID(1)
-// .then(results => {
-// console.log(results);
-// });
-
-// const Will = new User('Will Harris');
-// const Danny = new User('Danny Willis');
-
-// Will.greet(Danny);
-// Danny.greet(Will)
-
-// let u = User.findById(1);
-// u.name = 'Will';
-// u.save();
-
-// Todo.getByID(20000)
-// .then(results => {
-// console.log(results);
-// })
-
-
-
-
-
-// example of updating a row
-// function updatedCompleted(id, didComplete) {
-//     return db.result(`update todos
-//                         set completed=true           
-//                         where id=$1`, [id, didComplete]);
-// }
-
-
-// function markCompleted(id) {
-//     return updatedCompleted(id,true);
-
-// }
-
-
-
-// function markPending(id) {
-// //     // return updatedCompleted(id, false);
-// return db.result(`update todos
-//                         set completed=true           
-//                         where id=$1`, [id, false]);
-// }
-
-// markPending(1)
-// .then(result => {
-//     console.log(result); })
-
-
-
-
-
-// function updateName(id, name) {
-//     return db.result(`update todos
-//                     set name=$2
-//                     where id=$1`, [id, name])
-// }
-// updateName(2, 'buy car')
+// .then(todo =>{
+//     todo.updateName('sell the car')
 // .then(result =>{
 //     console.log(result);
 // })
-// // Delete
+// })
 
-// // function deleteByID(id) {
-// // return db.result(`delete from todos where id = $1`,[id])
-// // }
 
-// // deleteByID()
-// // .then(result =>{
-// // console.log(result.rowCount);
-// // })
+// Todo.getByID(1)
+// .then(usertodo => {
+//     usertodo.assignToUser(1)
+//     .then(result => {
+//         console.log(result);
+//         })
+//     })
+
+// Todo.getByID(1)
+// .then(todo =>{
+// todo.updatedCompleted(true)
+// .then(result =>{
+// console.log(result);
+// })
+// })
+
+
+// Todo.getByID(1)
+//     .then(todo =>{
+//         todo.markPending()
+//             .then(result => {
+//                     console.log(result); 
+//             })
+//         })
+
+// Todo.getByID(4)
+//     .then(todo =>{
+//         todo.markCompleted()
+//             .then(result => {
+//                     console.log(result); 
+//             })
+//         })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DELETE
+
+// Todo.getByID(7)
+// .then(todo =>{
+//     todo.deleteByID(7)
+//     .then(result =>{
+//         console.log(result);
+        
+//     })
+// })
+
+// Todo.deleteByID(8)
+// .then(result =>{
+//     console.log(result.rowCount);
+// })
+
