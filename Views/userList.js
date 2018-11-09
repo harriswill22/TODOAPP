9b2d915670bc0a2b6caf71cf5977902570588703
Will Harris <Harriswill22@yahoo.com>
2018-11-09 11:20:50 -0500

    makes view folder

---
+++ Views/userList.js
@@ -0,0 +1,19 @@
+function userToItem(userObject) {
+    return `
+    <li>${userObject.name}</li>
+    `;
+}
+
+
+
+
+function userList(arrayOfUsers) {
+    
+    const userItems  = arrayOfUsers.map(userToItem).join('');
+
+    return `
+    <ul>${userItems}</ul>
+    `;
+}
+
+module.exports = userList;
